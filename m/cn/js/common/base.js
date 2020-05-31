3c896ad6c84ff35c07727314915692e835769923
unknown <918800642@qq.com>
2020-05-31 18:56:29 +0800

    添加移动端

---
+++ m/cn/js/common/base.js
@@ -0,0 +1,85 @@
+
+function Base() {
+}
+Base.prototype = {
+    /**
+     * @function handleOpenNavigationItem 展开导航栏
+     */
+    handleNavigationIsExpand: function() {
+        $('.navigation-button').on('touchstart', function() {
+            var t = $(this),
+                    status = false,
+                    nav = $('.navigation-wrapper'),
+                    height = $(window).height() - $('#header').height(),
+                    body = $('body');
+
+            status = ( t.hasClass('active') ? true : false);
+            console.log(status)
+            if(status) {
+                nav.animate({ 'height' : '0' }, function() {
+                    nav.css('display', 'none');
+                    body.css({'overflow': 'auto', 'height' : 'auto'});
+                });
+                t.removeClass('active');
+            } else {
+                t.addClass('active');
+                nav.css('display', 'block').animate({ 'min-height' : height + 'px' });
+                body.css({'overflow': 'hidden', 'height' : '100%'});
+            }
+        })
+    },
+    /**
+     * @function handleOpenNavigationItem 展开二级菜单
+     */
+    handleOpenNavigationItem: function() {
+        $('.navigation-item').on('touchstart', function(e) {
+            console.log(123123)
+            e.stopPropagation();
+            var list     = $(this).children('.navigation-list');
+            var height   = 0;
+
+            console.log(list)
+            // 无子级不执行
+            if( list.length !== 0 ) {
+
+                // 计算展开的高度
+                height   = $(this).children('a').height() * list.find('.navigation-item').length;
+
+
+                if( list.hasClass('active') ) {
+                    list.css('display', 'none').animate({ 'height' : 0 }).removeClass('active');
+                    // list.siblings('i.el-icon-arrow-down').removeClass('rotate');
+                } else {
+                    setTimeout(() => {
+                        $(this).siblings('.navigation-item').find('.navigation-list').css('display', 'none').animate({ 'height' : 0 }).removeClass('active');
+                        list.css('display', 'block').addClass('active').animate({ 'height' : height + 'px' });
+                    }, 300);
+                    // list.siblings('i.el-icon-arrow-down').addClass('rotate');
+                };
+
+            };
+
+        });
+    },
+    /**
+     * @function gotoTop 回到顶部
+     */
+    gotoTop: function() {
+        $('#gotoTop').click(function(){
+                $('html ,body').animate({scrollTop: 0}, 300);
+            return false;
+        });
+    },
+    /**
+     * @function init 初始化页面里面的所有方法
+     */
+    init: function() {
+        // this.gotoTop()
+        this.handleNavigationIsExpand()
+        this.handleOpenNavigationItem()
+    }
+}
+var base = new Base()
+$(function(){
+	base.init();
+})
---
+++ m/cn/js/home/index.js
@@ -0,0 +1,102 @@
+
+function Home() {
+}
+Home.prototype = {
+    /**
+     * @function bannerSwiper 初始化首页轮播
+     */
+    bannerSwiper: function() {
+        console.log('初始化首页轮播')
+        var banner_swiper = new Swiper('.banner-swiper', {
+            speed:500,
+            autoplay: true,
+            loop: true,
+            // autoplayDisableOnInteraction : false,
+            // resistanceRatio : 0,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+                renderBullet: function (index, className) {
+                    console.log(index, className)
+                    return '<span class="' + className + '"></span>';
+                },
+            },
+        });
+        if($('.banner-swiper .swiper-slide').length < 2){
+            $('.swiper-pagination').hide();
+        }
+    },
+    /**
+     * @function productSwiper 初始化产品轮播
+     */
+    productSwiper: function() {
+        console.log('初始化首页轮播')
+        var banner_swiper = new Swiper('.product-swiper', {
+            speed:500,
+            autoplay: true,
+            loop: true,
+            // autoplayDisableOnInteraction : false,
+            // resistanceRatio : 0,
+            pagination: {
+                el: '.product-swiper-pagination',
+                clickable: true,
+                renderBullet: function (index, className) {
+                    console.log(index, className)
+                    return '<span class="' + className + '"></span>';
+                },
+            },
+        });
+        if($('.product-swiper .swiper-slide').length < 2){
+            $('.product-swiper-pagination').hide();
+        }
+    },
+    /**
+     * @function solutiontSwiper 初始化产品轮播
+     */
+    solutiontSwiper: function() {
+        console.log('初始化首页轮播')
+        var banner_swiper = new Swiper('.solution-swiper', {
+            // speed:500,
+            // autoplay: true,
+            // loop: true,
+            slidesPerView: 'auto',
+            spaceBetween: 10,
+            // autoplayDisableOnInteraction : false,
+            // resistanceRatio : 0,
+            pagination: {
+                el: '.solution-swiper-pagination',
+                clickable: true,
+                renderBullet: function (index, className) {
+                    console.log(index, className)
+                    return '<span class="' + className + '"></span>';
+                },
+            },
+        });
+        // if($('.product-swiper .swiper-slide').length < 2){
+        //     $('.product-swiper-pagination').hide();
+        // }
+    },
+    /**
+     * @function gotoTop 回到顶部
+     */
+    gotoTop: function() {
+        $('#gotoTop').click(function(){
+                $('html ,body').animate({scrollTop: 0}, 300);
+            return false;
+        });
+    },
+    /**
+     * @function init 初始化页面里面的所有方法
+     */
+    init: function() {
+        console.log('初始化页面')
+        this.bannerSwiper()
+        this.productSwiper()
+        this.solutiontSwiper()
+        this.gotoTop()
+    }
+}
+var home = new Home()
+$(function(){
+	home.init();
+})
