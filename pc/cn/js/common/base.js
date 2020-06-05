0cd2a2ae756b8342b67bc2c6a00a5be24535d5d5
penggang <918800642@qq.com>
2020-06-05 18:12:46 +0800

    修改接口

--- pc/cn/js/common/base.js
+++ pc/cn/js/common/base.js
@@ -32,22 +32,41 @@ Base.prototype = {
      */
     formSubmit: function() {
         $('.submitBtn').click(function(){
-            var company = $('.company').val()
-            var email = $('.email').val()
-            var message = $('.remark').val()
-            var mobile = $('.tel').val()
-            var name = $('.name').val()
-            console.log(11111)
+            var company = $('.company input').val()
+            var email = $('.email input').val()
+            var message = $('.remark input').val()
+            var mobile = $('.tel input').val()
+            var name = $('.name input').val()
+            if (!name) {
+                alert('请填写姓名')
+                return false
+            }
+            if (!mobile) {
+                alert('请填写手机号')
+                return false
+            }
+            var phone1Reg = /^1[3456789]\d{9}$/
+            if (!(phone1Reg.test(mobile))) {
+                alert('请填写正确的联系方式')
+                return false
+            }
+            var phone2Reg = /(\d{4}-)?\d{6,8}/
+            if (!(phone2Reg.test(mobile))) {
+                alert('请填写正确的联系方式')
+                return false
+            }
+            var params = {
+                "company": company, "email": email, "message": message, "mobile": mobile, "name": name
+            }
             $.ajax({
-                type: 'post',
+                type: 'POST',
                 async: false,
-                url: "http://api/shuhezhitong.com/sales/insert",
-                data: { company, email, message, mobile, name},
-                dataType: 'jsonp',
-                jsonp:"jsoncallback",
-                jsonpCallback:"callback",
+                url: "http://api.shuhezhitong.com/sales/insert",
+                data: JSON.stringify(params),
+                dataType: 'json',
+                contentType: "application/json;charset=utf-8",
                 success: function (data) {
-                    console.log(data)
+                    $('.mask').hide()
                 }
             });
         })
